c5a4cc899f650a087f580503bebea6fbc771947a
Dave Arnold <darnold@hashicorp.com>
2018-06-22 14:09:13 -0700

    updating module

--- variables.tf
+++ variables.tf
@@ -2,58 +2,59 @@
 // Vault settings
 //-------------------------------------------------------------------
 
-variable "download-url" {
-    default = "https://releases.hashicorp.com/vault/0.10.3/vault_0.10.3_linux_amd64.zip"
-    description = "URL to download Vault"
+variable "download_url" {
+  default     = "https://releases.hashicorp.com/vault/0.10.3/vault_0.10.3_linux_amd64.zip"
+  description = "URL to download Vault"
 }
 
 variable "config" {
-    description = "Configuration (text) for Vault"
+  description = "Configuration (text) for Vault"
 }
 
-variable "extra-install" {
-    default = ""
-    description = "Extra commands to run in the install script"
+variable "extra_install" {
+  default     = ""
+  description = "Extra commands to run in the install script"
 }
 
 //-------------------------------------------------------------------
 // AWS settings
 //-------------------------------------------------------------------
 
-variable "ami" {
-    default = "ami-7eb2a716"
-    description = "AMI for Vault instances"
+variable "availability_zone" {
+  default     = "us-east-1a"
+  description = "Availability zones for launching the Vault instances"
 }
 
-variable "availability-zones" {
-    default = "us-east-1a,us-east-1b"
-    description = "Availability zones for launching the Vault instances"
-}
-
-variable "elb-health-check" {
-    default = "HTTP:8200/v1/sys/health"
-    description = "Health check for Vault servers"
+variable "elb_health_check" {
+  default     = "HTTP:8200/v1/sys/health"
+  description = "Health check for Vault servers"
 }
 
 variable "instance_type" {
-    default = "m3.medium"
-    description = "Instance type for Vault instances"
+  default     = "m3.medium"
+  description = "Instance type for Vault instances"
 }
 
-variable "key-name" {
-    default = "default"
-    description = "SSH key name for Vault instances"
+variable "key_name" {
+  default     = "default"
+  description = "SSH key name for Vault instances"
 }
 
-variable "nodes" {
-    default = "2"
-    description = "number of Vault instances"
+variable "servers" {
+  default     = "3"
+  description = "number of Vault instances"
 }
 
-variable "subnets" {
-    description = "list of subnets to launch Vault within"
+variable "subnet" {
+  description = "list of subnets to launch Vault within"
 }
 
-variable "vpc-id" {
-    description = "VPC ID"
+variable "vpc_id" {
+  description = "VPC ID"
+}
+
+variable "resource_tags" {
+  type        = "map"
+  default     = {}
+  description = "Resource Tags. Get applied anywhere tags can be applied"
 }

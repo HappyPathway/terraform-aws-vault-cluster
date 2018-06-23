1b639e5b11c1afadd59bbaada9d1948b72f0627a
Dave Arnold <darnold@hashicorp.com>
2018-06-22 22:18:05 -0700

    adding more vars

--- instance-policy/variables.tf
+++ instance-policy/variables.tf
@@ -1,3 +1,9 @@
 variable "environment_name" {}
 variable "region" {}
 variable "kms_arn" {}
+
+variable "resource_tags" {
+  default     = {}
+  type        = "map"
+  description = "Optional map of config tags"
+}

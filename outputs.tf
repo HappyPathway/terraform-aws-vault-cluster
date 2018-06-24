222d6beb1dcc6e101cf00a43030be1009d5388cd
Dave Arnold <darnold@hashicorp.com>
2018-06-24 10:31:24 -0700

    wrapping vault init in function and only executing if token doesn't exist in consul

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,17 @@
 output "address" {
-    value = "${aws_elb.vault.dns_name}"
+  value = "${aws_elb.vault.dns_name}"
 }
 
 // Can be used to add additional SG rules to Vault instances.
 output "vault_security_group" {
-    value = "${aws_security_group.vault.id}"
+  value = "${aws_security_group.vault.id}"
 }
 
 // Can be used to add additional SG rules to the Vault ELB.
 output "elb_security_group" {
-    value = "${aws_security_group.elb.id}"
+  value = "${aws_security_group.elb.id}"
+}
+
+output "cluster_hash" {
+  value = "${random_id.environment_name.hex}"
 }
